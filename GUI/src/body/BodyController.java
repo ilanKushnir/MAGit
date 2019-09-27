1e172abe5b19dc5b9d8b8686994224890feeb9b3
Ofir Cohen <ofirch@mta.ac.il>
2019-09-27 16:25:55 +0300

    allgood

--- GUI/src/body/BodyController.java
+++ GUI/src/body/BodyController.java
@@ -276,9 +276,6 @@ public class BodyController {
         addCommitsToGraph(commitsTree, allCommits);
         addEdgesToGraph(commitsTree);
         commitsTree.endUpdate();
-        //addBranchesToGraph(tree);
-        /////////setTreePositioning(tree.getModel(), Branch.allBranchesToList());
-
 
         commitsTree.layout(new CommitTreeLayout());
 
@@ -363,71 +360,4 @@ public class BodyController {
         return out;
     }
 
-
-//    private void setTreePositioning(Model model, List<Branch> branchesTreeOrdered) {
-//        List<String> usedSha1 = new LinkedList<>();
-//        for (ICell cell : model.getAllCells()) {
-//            CommitNode commitNode = (CommitNode) cell;
-//            for (int i = 0; i < branchesTreeOrdered.size(); i++) {
-//                List<Commit.commitComps> commits = Branch.getAllCommits(branchesTreeOrdered.get(i));
-//                for (Commit.commitComps c : commits) {
-//                    if (c.getSha1().equals(commitNode.getSha1())) {
-//                        if (usedSha1.contains(c.getSha1()))
-//                            commitNode.setPos(i);
-//                        else {
-//                            usedSha1.add(c.getSha1());
-//                            commitNode.setPos(i);
-//                            break;
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        if (model.getAllCells().size() > 0) {
-//            ICell firstCommit = model.getAllCells().get(model.getAllCells().size() - 1);
-//            CommitNode node = (CommitNode) firstCommit;
-//            node.setPos(0);
-//        }
-//    }
-
-//
-//    private void createCommitNodes() {
-//        final Model model = commitsTree.getModel();
-//
-//        commitsTree.beginUpdate();
-//
-//
-//        ICell c1 = new CommitNode("20.07.2019 | 22:36:57", "Menash", "initial commit");
-//        ICell c2 = new CommitNode("21.07.2019 | 22:36:57", "Moyshe Ufnik", "developing some feature");
-//        ICell c3 = new CommitNode("20.08.2019 | 22:36:57", "Old Majesty, The FU*!@N Queen of england", "A very long commit that aims to see if and where the line will be cut and how it will look a like... very Interesting");
-//        ICell c4 = new CommitNode("20.09.2019 | 13:33:57", "el professore", "yet another commit");
-//        ICell c5 = new CommitNode("30.10.2019 | 11:36:54", "bella chao", "merge commit of 'yet another commit' and other commit");
-//
-//        model.addCell(c1);
-//        model.addCell(c2);
-//        model.addCell(c3);
-//        model.addCell(c4);
-//        model.addCell(c5);
-//
-//        final Edge edgeC12 = new Edge(c1, c2);
-//        model.addEdge(edgeC12);
-//
-//        final Edge edgeC23 = new Edge(c2, c4);
-//        model.addEdge(edgeC23);
-//
-//        final Edge edgeC45 = new Edge(c4, c5);
-//        model.addEdge(edgeC45);
-//
-//        final Edge edgeC13 = new Edge(c1, c3);
-//        model.addEdge(edgeC13);
-//
-//        final Edge edgeC35 = new Edge(c3, c5);
-//        model.addEdge(edgeC35);
-//
-//        commitsTree.endUpdate();
-//
-//        commitsTree.layout(new CommitTreeLayout());
-//
-//    }
 }
