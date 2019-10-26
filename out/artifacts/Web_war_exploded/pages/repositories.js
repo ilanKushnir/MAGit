c99c4d91640bd22535fb918fad8849e0f5d7b902
Ofir Cohen <ofirch@mta.ac.il>
2019-10-26 13:02:28 +0300

    co operating merge

---
+++ out/artifacts/Web_war_exploded/pages/repositories.js
@@ -0,0 +1,318 @@
+var CURRENT_USER_DATA_URL = buildUrlWithContextPath("currentUserInformation");
+var OTHER_USERS_DATA_URL = buildUrlWithContextPath("otherUsersInformation");
+var NEW_REPOSITORY_URL = buildUrlWithContextPath("newRepository");
+var FORK_REPOSITORY_URL = buildUrlWithContextPath("forkRepository");
+var REPOSITORY_INFO_URL = buildUrlWithContextPath("repositoryInformation");
+var LOAD_REPOSITORY_URL = buildUrlWithContextPath("loadRepository");
+var CURRENT_USER_DATA;
+var OTHER_USERS_DATA;
+
+
+$(function () {
+    initializeWindow();
+});
+
+function initializeWindow() {
+    refreshCurrentUserData();
+    refreshOtherUsersData();
+    console.log()
+}
+
+
+
+
+function updateUsernamePlaceholders() {
+    var currentUserName = CURRENT_USER_DATA.userName;
+    $("#topBarUsername").text(currentUserName);
+    $("#currentUserReposTableTitle").text(currentUserName + "'s repositories in M.A.Git");
+}
+
+
+
+function refreshCurrentUserData() {
+    ajaxCurrentUserData(function (currentUserData) {
+        setCurrentUserDataVar(currentUserData);
+        displayCurrentUserRepositories();
+        displaySideMenuRepoLinks();
+        updateUsernamePlaceholders();
+    });
+}
+function setCurrentUserDataVar(currentUserData) {
+    CURRENT_USER_DATA = currentUserData;
+}
+function ajaxCurrentUserData(callback) {
+    $.ajax({
+        url: CURRENT_USER_DATA_URL,
+        dataType: "json",
+        success: function (currentUserData) {
+            callback(currentUserData);
+        }
+    });
+}
+function displayCurrentUserRepositories() {
+    $("#currentUserReposTable").empty();
+    $.each(CURRENT_USER_DATA.repositoriesDataList || [], addSingleRepositoryDataToCurrentUser);
+}
+function addSingleRepositoryDataToCurrentUser(index, currentUserSingleRepositoryData) {
+    var singleRepositoryDataRow = createCurrentUserSingleRepositoryDataHTML(currentUserSingleRepositoryData);
+    $(singleRepositoryDataRow).on('click-row.bs.table', function (e, row, $element) {
+        loadRepository(currentUserSingleRepositoryData.name);
+    });
+    $("#currentUserReposTable").append(singleRepositoryDataRow);
+
+}
+function createCurrentUserSingleRepositoryDataHTML(currentUserSingleRepositoryData) {
+    return  $("<tr>\n" +
+                "<td>" + currentUserSingleRepositoryData.name + "</td>\n" +
+                "<td>" + currentUserSingleRepositoryData.activeBranchName + "</td>\n" +
+                "<td>" + currentUserSingleRepositoryData.numberOfBranches + "</td>\n" +
+                "<td>" + currentUserSingleRepositoryData.lastCommitDate + "</td>\n" +
+                "<td>" + currentUserSingleRepositoryData.lastCommitMessage + "</td>" +
+            "</tr>\n");
+}
+
+// TODO user repository make it clickable with link
+
+
+
+
+function loadRepository(repositoryName) {
+    ajaxLoadRepository(repositoryName, ShowModal);
+}
+
+function ajaxLoadRepository(repositoryName, callback) {
+    $.ajax({
+        url: LOAD_REPOSITORY_URL,
+        data:{
+            repositoryName: repositoryName
+        },
+        success: (message) => {
+            var response = JSON.parse(message);
+            if(response.success === false) {
+                callback(response)
+            }
+        }
+    });
+}
+
+
+
+
+//  SIDE MENU
+function displaySideMenuRepoLinks() {
+    $.each(CURRENT_USER_DATA.repositoriesDataList || [], addSingleRepoSideMenuLink);
+}
+
+function addSingleRepoSideMenuLink(index, currentUserSingleRepositoryData) {
+    if (!$("#side-menu-repo-links").find('#' + replaceSpacesWithUndersore(currentUserSingleRepositoryData.name) + '-side-link').length) {
+        var singleRepositoryData = createSideMenuSingleRepositoryLink(currentUserSingleRepositoryData);
+        singleRepositoryData.on("click", function() {
+            loadRepository(currentUserSingleRepositoryData.name);
+        });
+        $("#side-menu-repo-links").append(singleRepositoryData);
+    }
+}
+
+function createSideMenuSingleRepositoryLink(currentUserSingleRepositoryData){
+    return  $('<li class="nav-item" role="presentation" id="' + replaceSpacesWithUndersore(currentUserSingleRepositoryData.name) + '-side-link">  '  +
+            '    <a class="nav-link" href="repository.html" style="padding-top: 5px;padding-bottom: 5px;padding-left: 30px;">  '  +
+            '        <i class="fas fa-tachometer-alt"></i>  '  +
+            '        <span>' +
+                         currentUserSingleRepositoryData.name +
+            '        </span>  '  +
+            '    </a>  '  +
+            '</li>  ' );
+}
+// TODO side menu user repository make it clickable with link
+
+
+
+
+
+
+
+
+
+
+
+//  LOWER TABLE (other users date)
+function refreshOtherUsersData() {
+    ajaxOtherUsersData(function (currentUserData) {
+        setOtherUsersDataVar(currentUserData);
+        displayOtherUsersButtons()
+    });
+}
+function ajaxOtherUsersData(callback) {
+    $.ajax({
+        url: OTHER_USERS_DATA_URL,
+        dataType: "json",
+        success: function (currentUserData) {
+            callback(currentUserData);
+        }
+    });
+}
+function setOtherUsersDataVar(otherUsersData) {
+    OTHER_USERS_DATA = otherUsersData;
+}
+function displayOtherUsersButtons() {
+    console.log(OTHER_USERS_DATA);
+    $.each(OTHER_USERS_DATA || [], addOtherUserButton);
+}
+function addOtherUserButton(index, otherUserData) {
+    if (!$("#otherUsersList").find('#' + replaceSpacesWithUndersore(otherUserData.userName) + '-row').length) {
+        var otherUserButton = createOtherUserSingleButtonHTML(otherUserData);
+        $("#otherUsersList").append(otherUserButton);
+    }
+    updateOtherUserRepositoryButtons(otherUserData);
+}
+function createOtherUserSingleButtonHTML(otherUserData) {
+    var collapseContentID = replaceSpacesWithUndersore(otherUserData.userName) + '-collapse';
+    return  '<tr ' + 'id="' + replaceSpacesWithUndersore(otherUserData.userName) + '-row"> '  +
+            ' <td>  '  +
+            ' <div>  '  +
+            '     <a class="btn btn-light" data-toggle="collapse" aria-expanded="false" href="#' + collapseContentID + '" role="button">  '  +
+                      otherUserData.userName  +
+            '     </a>  '  +
+            '     <div class="collapse" ' + 'id="' + replaceSpacesWithUndersore(otherUserData.userName) + '-collapse">'  +
+            '     </div>  '  +
+            ' </div>  '  +
+            ' </td>  '  +
+            '</tr>  ' ;
+}
+function updateOtherUserRepositoryButtons(otherUserData) {
+    $('#' + replaceSpacesWithUndersore(otherUserData.userName) + '-collapse').empty();
+    otherUserData.repositoriesDataList.forEach(function(element) {
+        var forkButton = createSingleOtherUserRepositoryForkButton(element.name, otherUserData.userName);
+        var collapseContentID = replaceSpacesWithUndersore(otherUserData.userName) + '-collapse';
+        forkButton.on("click", function() {
+            forkRepository(otherUserData.userName, element.name);
+        })
+        $('#' + collapseContentID).append(forkButton);
+    });
+}
+
+function createSingleOtherUserRepositoryForkButton(repositoryName, username) {  // returns an elemene beacuse of the $ - so can be appended easily later
+    return  $('<a class="btn btn-light btn-icon-split" role="button" style="margin-top: 11px;" id="' + replaceSpacesWithUndersore(username) + '-' + replaceSpacesWithUndersore(repositoryName) + '-forkButton">  '  +
+            '    <span class="text-black-50 icon">  '  +
+            '        <i class="far fa-copy"></i>  '  +
+            '    </span>  '  +
+            '    <span class="text-dark text">  '  +
+                    repositoryName  +
+            '    </span>  '  +
+            '</a>  ');
+}
+function fork(otherUserData, remoteRepository) {
+
+}
+
+
+
+
+
+
+
+
+function addNewRepositoryToCurrentUser(event) {
+    var file = event.target.files[0];
+    ajaxNewRepository(file, ShowModal);
+}
+
+function ajaxNewRepository(file, callback) {
+    var reader = new FileReader();
+    reader.onload = function () {
+        var content = reader.result;
+        $.ajax(
+            {
+                url: NEW_REPOSITORY_URL,
+                data: {
+                    file: content
+                },
+                type: 'POST',
+                success: (message) => {
+                    var response = JSON.parse(message);
+                    repositoryAjaxSucceededCallback(response)}
+            }
+        );
+    };
+    reader.readAsText(file);
+
+    function repositoryAjaxSucceededCallback(response) {
+        ShowModal(response);
+
+        refreshCurrentUserData();
+        $('#fileChooser').val(null);
+    }
+}
+
+
+
+
+
+
+
+function forkRepository(otherUsername, repositoryName) {
+    ajaxForkRepository(otherUsername, repositoryName, ShowModal);
+}
+
+function ajaxForkRepository(otherUsername, otherUserRepositoryName, callback) {
+    $.ajax({
+        url: FORK_REPOSITORY_URL,
+        data:{
+            otherUsername : otherUsername,
+            otherUserRepositoryName: otherUserRepositoryName
+        },
+        success: (message) => {
+            var response = JSON.parse(message);
+            callback(response)
+        }
+    });
+}
+
+
+
+
+
+
+function watchRepositoryInfo(repositoryName) {
+    ajaxWatchRepositoryInfo(repositoryName);
+}
+function ajaxWatchRepositoryInfo(repositoryName) {
+    $.ajax({
+        url: REPOSITORY_INFO_URL,
+        dataType:"json",
+        data:{
+            repositoryNameToWatch : repositoryName
+        },
+        success:function(newUrl){
+            var fullUrl = buildUrlWithContextPath(newUrl);
+            window.location.replace(fullUrl);
+        }
+    });
+
+}
+
+
+
+
+
+
+
+
+function ShowModal(response) {
+    if (response.success) {
+        document.getElementById("modal-success-content").textContent = response.message;
+        $('#successModal').modal('show');
+    } else {
+        document.getElementById("modal-failure-content").textContent = response.message;
+        $('#failureModal').modal('show');
+    }
+}
+
+function replaceSpacesWithUndersore(str){
+    return str !== undefined ? str.replace(/ /g,"_") : str;
+}
+
+$(function () {
+    setInterval(refreshOtherUsersData, 2000);
+    setInterval(refreshCurrentUserData, 2000);
+});
---
+++ out/artifacts/Web_war_exploded/pages/repository.js
@@ -0,0 +1,261 @@
+var CURRENT_USER_DATA_URL = buildUrlWithContextPath("currentUserInformation");
+var CURRENT_REPOSITORY_DATA_URL = buildUrlWithContextPath("currentRepositoryInformation");
+var CHECKOUT_URL = buildUrlWithContextPath("checkout");
+var WORKING_COPY_URL = buildUrlWithContextPath("workingCopy");
+
+var CURRENT_USER_DATA;
+var CURRENT_REPOSITORY_DATA;
+var WORKING_COPY_LIST;
+
+$(function () {
+    initializeWindow();
+});
+
+function initializeWindow() {
+    refreshCurrentUserData();
+    refresRepositoryData();
+}
+
+// USER DATA
+function refreshCurrentUserData() {
+    ajaxCurrentUserData(function (currentUserData) {
+        CURRENT_USER_DATA = currentUserData;
+        $("#topBarUsername").text( CURRENT_USER_DATA.userName);
+        displaySideMenuRepositories(currentUserData);
+    });
+}
+
+function ajaxCurrentUserData(callback) {
+    $.ajax({
+        url: CURRENT_USER_DATA_URL,
+        dataType: "json",
+        success: function (currentUserData) {
+            callback(currentUserData);
+        }
+    });
+}
+
+function displaySideMenuRepositories(currentUserData) {
+    $.each(currentUserData.repositoriesDataList || [] , addSingleRepoSideMenuLink)
+}
+
+function addSingleRepoSideMenuLink(index, currentUserSingleRepositoryData) {
+    if (!$("#side-menu-repo-links").find('#' + replaceSpacesWithUndersore(currentUserSingleRepositoryData.name) + '-side-link').length) {
+        var singleRepositoryData = createSideMenuSingleRepositoryLink(currentUserSingleRepositoryData);
+        singleRepositoryData.on("click", function() {
+            loadRepository(currentUserSingleRepositoryData.name);
+        });
+        $("#side-menu-repo-links").append(singleRepositoryData);
+    }
+}
+
+function createSideMenuSingleRepositoryLink(currentUserSingleRepositoryData) {
+    return  $('<li class="nav-item" role="presentation" id="' + replaceSpacesWithUndersore(currentUserSingleRepositoryData.name) + '-side-link">  '  +
+        '    <a class="nav-link" href="repository.html" style="padding-top: 5px;padding-bottom: 5px;padding-left: 30px;">  '  +
+        '        <i class="fas fa-tachometer-alt"></i>  '  +
+        '        <span>' +
+        currentUserSingleRepositoryData.name +
+        '        </span>  '  +
+        '    </a>  '  +
+        '</li>  ' );
+}
+
+//  REPOSITORY DATA
+function refresRepositoryData() {
+    ajaxRepositoryData(function (data) {
+        CURRENT_REPOSITORY_DATA = data;
+        $("#shown-repo-headline").text(data.name);
+        displayBranchesCheckoutButtons();
+        refreshForkedRepositoriesTable();
+        refreshCommitsTable();
+        refreshWorkingCopyList();
+    });
+}
+
+function ajaxRepositoryData(callback) {
+    $.ajax({
+        url: CURRENT_REPOSITORY_DATA_URL,
+        dataType: "json",
+        success: function (currentRepositoryData) {
+            callback(currentRepositoryData);
+        }
+    });
+}
+
+function refreshCommitsTable() {
+    $("#commitsDataTable").empty();
+    $.each(CURRENT_REPOSITORY_DATA.commitsList || [], addSingleCommitRow);
+}
+
+function displayBranchesCheckoutButtons() {
+    $("#branchCheckoutButtons").empty();
+    $.each(CURRENT_REPOSITORY_DATA.branchesDataList || [], addSingleBranchCheckoutButton);
+}
+
+function refreshForkedRepositoriesTable() {
+    $("#forkedRepositoriesTable").empty();
+    $.each(CURRENT_REPOSITORY_DATA.forkedMap || [], addSingleForkedRepositoryRow)
+}
+
+function refreshWorkingCopyList() {
+    ajaxWorkingCopy("refreshWC", function(workingCopyData) {
+        WORKING_COPY_LIST = workingCopyData;
+        $("#workingCopyList").empty();
+        $.each(WORKING_COPY_LIST.components || [], addSingleWorkingCopyComponent);
+    });
+}
+
+function ajaxWorkingCopy(action, callback) {
+    $.ajax({
+        url: WORKING_COPY_URL,
+        dataType: "json",
+        data: {
+            wcAction: action
+        },
+        success: function (response) {
+            callback(response);
+        }
+    });
+}
+
+function addSingleCommitRow(index, commitData) {
+    let singleCommitRow = createSingleCommitRow(commitData);
+    $("#commitsDataTable").append(singleCommitRow);
+}
+
+function addSingleBranchCheckoutButton(index, branchData) {
+    let branchCheckoutButtonHTML = createBranchCheckoutButton(branchData);
+    $(branchCheckoutButtonHTML).on('click', function () {
+        console.log("checkout to " + branchData.name);
+    })
+    $("#branchCheckoutButtons").append(branchCheckoutButtonHTML);
+}
+
+function addSingleForkedRepositoryRow(key, value) {
+    let forkedRepoRow = createForkedRepositoryRow(key, value);
+    $("#forkedRepositoriesTable").append(forkedRepoRow);
+}
+
+function addSingleWorkingCopyComponent(index, componentData) {
+    let singleWorkingCopyRow = createSingleWorkingCopyRow(componentData);
+    $("#workingCopyList").append(singleWorkingCopyRow);
+}
+
+function createSingleCommitRow(commitData) {
+    let tableRow =  $('   <tr>  '  +
+        ' <td>' + commitData.SHA1 + ' </td>  '  +
+        ' <td> ' + commitData.message + ' </td>  '  +
+        ' <td> ' + commitData.dateCreated + ' </td>  '  +
+        ' <td> ' + commitData.author + ' </td>  '  +
+        '</tr> ' );
+    addPointingBranchesToCommitRow(tableRow, commitData);
+
+    return tableRow;
+}
+
+function addPointingBranchesToCommitRow(commitRow, commitData) {
+    $.each(commitData.pointingBranches || [] , (branch) => {
+        commitRow.append(
+            $('<td><button class="btn btn-light" type="button" style="margin: 3px;font-size: 10px;padding-top: 3px;padding-right: 6px;padding-bottom: 3px;padding-left: 6px;" disabled="">'
+                + branch
+                + '</button></td>  '));
+    });
+}
+
+function createBranchCheckoutButton(branchData) {
+    let btnClass;
+    var disabled = false;
+
+    if (CURRENT_REPOSITORY_DATA.activeBranchName === branchData.name) {
+        disabled = true;
+        if (branchData.isRtb) {
+            btnClass = "btn btn-outline-warning";
+        } else {
+            btnClass = "btn btn-warning";
+        }
+    } else if (branchData.name === "master") {
+        if (branchData.isRtb) {
+            btnClass = "btn btn-outline-success"
+        } else {
+            btnClass = "btn btn-success";
+        }
+    } else {
+        if (branchData.isRtb) {
+            btnClass = "btn btn-outline-secondary";
+        } else {
+            btnClass = "btn btn-secondary";
+        }
+    }
+
+    let btn = $("<button type='button' style='margin: 5px;'>" + branchData.name + "</button>\n");
+    if (disabled) {
+        btn.attr("disabled", "disabled").button("refresh");
+    }
+    btn.on("click", function () {
+        checkout(branchData.name);
+    });
+    return btn.addClass(btnClass);
+}
+
+function createSingleWorkingCopyRow(componentData) {
+    var icon = (componentData.type === "folder") ? "<i class=\"fas fa-folder-open\"></i>" : "<i class=\"far fa-file-alt\"></i>";
+    var spaces = "";
+    // TODO add indentation
+
+    let btn = $(
+        '<tr>'  +
+        '   <td>  '  +
+        '       <a href="#">' +
+        spaces + icon + '  ' + componentData.name +
+        '       </a></td>  '  +
+        '   <td class="text-center">  '  +
+        '       <button class="btn btn-danger btn-circle ml-1" type="button">  '  +
+        '           <i class="fas fa-trash text-white"></i>  '  +
+        '       </button>  '  +
+        '       <button class="btn btn-warning btn-circle ml-1" type="button">  '  +
+        '           <i class="fas fa-edit text-white"></i>  '  +
+        '       </button>  '  +
+        '   </td>  '  +
+        '</tr>'
+    );
+    return btn;
+}
+
+function createForkedRepositoryRow(key, value) {
+    return  '<tr>' +
+            '<td>' + value + '</td>'  +
+            '<td>' + key + '</td>'  +
+            '</tr>';
+}
+
+function replaceSpacesWithUndersore(str){
+    return str !== undefined ? str.replace(/ /g,"_") : str;
+}
+
+
+// manager functions
+function checkout(branchName) {
+    var reader = new FileReader();
+
+        $.ajax(
+            {
+                url: CHECKOUT_URL,
+                dataType: "json",
+                data: {
+                    branchToCheckout: branchName
+                },
+                success: (message) => {
+                    checkoutCallback(message)
+                }
+            }
+        );
+
+    function checkoutCallback(message) {
+        // var jsonResponse = JSON.parse(message);
+        if (message.success) {
+            refresRepositoryData();
+        } else {
+            ShowModal(message);
+        }
+    }
+}
